cf0ed87553de7ce817bcd0544eb654b4a6552296
youssra123 <fadlaouiyoussra@gmail.com>
2021-02-12 11:23:45 +0100

    First commit1

---
+++ src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,24 @@
+package org.sid.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+
+    DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient r, DiscoveryLocatorProperties p){
+        return new DiscoveryClientRouteDefinitionLocator(r,p);
+    }
+}
